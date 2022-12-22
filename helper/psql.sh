b41fdcfa292cb4b3ebdb0fd8297117d6230c4e61
Tim Baber <tim.baber@neuralink.com>
2022-12-22 17:00:43 -0600

    testing psql scripting

--- helper/psql.sh
+++ helper/psql.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-psql -U postgres
+# Set the user and password variables
+USERNAME=maas
+PASSWORD=maas
 
-create user neuralink with encrypted password 'neuralink';
+# Set the database name
+DATABASE=maas
 
-create database maas;
+# Connect to the default PostgreSQL database
+psql -U postgres <<EOF
 
-grant all privileges on database maas to neuralink;
+# Create the new user
+CREATE USER $USERNAME WITH ENCRYPTED PASSWORD '$PASSWORD';
+
+# Create the new database
+CREATE DATABASE $DATABASE WITH OWNER $USERNAME;
+
+# Grant all privileges on the database to the user
+GRANT ALL PRIVILEGES ON DATABASE $DATABASE TO $USERNAME;
+
+EOF
