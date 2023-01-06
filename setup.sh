d20b2a8b08364baf2d6c3c72f506a6bddf36c1d8
Tim Baber <tim.baber@neuralink.com>
2023-01-06 12:45:15 -0600

    testing maas init

--- setup.sh
+++ setup.sh
@@ -2,9 +2,11 @@
 
 # Invoke & run bootstrap.sh
 # Updates packages, installs postgres, snapd, maas, docker
-echo  🧰 - bootstrapping platform services...
+echo  🧰 - Installing platform services...
 ./helper/bootstrap.sh
 echo  ✅ - Success - proceeding with postgres setup...
 # Invoke & run psql.sh
 # Create postgres database, user and assign role to user
 ./helper/psql.sh
+echo  ✅ - Success - invoking MaaS regiond + rackd setup
+sudo maas init region+rack --database-uri "postgres://maas:maas@localhost/maas"
